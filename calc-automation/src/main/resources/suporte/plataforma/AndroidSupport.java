766e6bcf2b62b05ebd5ca036d7c68897aef51db1
Wilson Amorim Neto <wamorimn@everis.com>
2019-10-25 23:07:23 -0300

    Refector calc

---
+++ calc-automation/src/main/resources/suporte/plataforma/AndroidSupport.java
@@ -0,0 +1,31 @@
+package suporte.plataforma;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import suporte.driver.ThreadDriver;
+
+public class AndroidSupport implements IPlatform {
+
+    public WebDriverWait wait;
+
+    public void setupAppium() throws IOException {
+
+		DesiredCapabilities caps = new DesiredCapabilities();
+
+		caps.setCapability("platformName", "Android");
+		caps.setCapability("deviceName", "Android");
+		caps.setCapability("automationName", "UiAutomator2");
+		caps.setCapability("appPackage", "com.android.calculator2");
+		caps.setCapability("appActivity", "com.android.calculator2.Calculator");
+		caps.setCapability("autoGrantPermissions", true);
+		
+		ThreadDriver.setTDriver(new AndroidDriver<MobileElement>(new URL("http://localhost:4723/wd/hub"), caps));
+		wait = new WebDriverWait(ThreadDriver.getTDriver(), 10);
+    }
+}
